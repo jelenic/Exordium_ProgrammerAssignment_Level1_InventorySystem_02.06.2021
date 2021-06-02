b7905b27d0764e9a2a5aa04f4454eacbdab49585
Jakov Jelenić <jakov.jelenic@fer.hr>
2021-06-02 19:57:06 +0200

    basic movement and animation

---
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = 5f;
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    // Update is called once per frame, fixed update is called every one unit of time regaardless of framerate
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        if (movement.x < 0)
+        {
+            this.transform.localScale = new Vector3(-1,1,1);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
